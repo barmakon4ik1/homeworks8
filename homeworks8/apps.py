0f632e58a452a95fb9cf35892c660e19b9a33461
barmakon4ik <barmakon4ik@gmail.com>
2024-08-02 07:48:50 +0200

    Model Howeworks8 added

---
+++ homeworks8/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Homeworks8Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'homeworks8'
---
+++ homeworks8/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+TASK_CHOICES = [
+    ('N', 'New'),
+    ('I', 'In Progress'),
+    ('P', 'Pending'),
+    ('B', 'Blocked'),
+    ('D', 'Done'),
+]
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Наименование задачи')
+    description = models.TextField(verbose_name="Описание задачи")
+    categories = models.ManyToManyField('Category')
+    status = models.CharField(max_length=50, choices=TASK_CHOICES, verbose_name="Тип задачи")
+    deadline = models.DateField(verbose_name="Срок выполнения")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время начала выполнения")
+
+    def __str__(self):
+        return self.title
+
+
+class SubTask(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Наименование подзадачи')
+    description = models.TextField(verbose_name="Описание подзадачи")
+    task = models.ForeignKey('Task', on_delete=models.CASCADE, verbose_name='Основная задача')
+    status = models.CharField(max_length=50, choices=TASK_CHOICES, verbose_name="Тип подзадачи")
+    deadline = models.DateField(verbose_name="Срок выполнения")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время начала выполнения")
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Наименование категории")
+
+    def __str__(self):
+        return self.name
