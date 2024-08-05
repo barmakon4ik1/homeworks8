e90eedcb57b9e8cbf0627ce78b00b1811354fb33
Barmakon4ik <barmakon4ik1@gmail.com>
2024-08-05 20:02:43 +0200

    homeworks9 added

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'homeworks8.apps.Homeworks8Config',
 ]
 
 MIDDLEWARE = [
--- homeworks8/models.py
+++ homeworks8/models.py
@@ -10,7 +10,7 @@ TASK_CHOICES = [
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=100, verbose_name='Наименование задачи')
+    title = models.CharField(max_length=100, verbose_name='Наименование задачи', unique=True)
     description = models.TextField(verbose_name="Описание задачи")
     categories = models.ManyToManyField('Category')
     status = models.CharField(max_length=50, choices=TASK_CHOICES, verbose_name="Тип задачи")
@@ -20,9 +20,14 @@ class Task(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        db_table = 'task_manager_task'
+        ordering = ['-created_at']
+        verbose_name = 'Task'
+
 
 class SubTask(models.Model):
-    title = models.CharField(max_length=100, verbose_name='Наименование подзадачи')
+    title = models.CharField(max_length=100, verbose_name='Наименование подзадачи', unique=True)
     description = models.TextField(verbose_name="Описание подзадачи")
     task = models.ForeignKey('Task', on_delete=models.CASCADE, verbose_name='Основная задача')
     status = models.CharField(max_length=50, choices=TASK_CHOICES, verbose_name="Тип подзадачи")
@@ -31,6 +36,12 @@ class SubTask(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        db_table = 'task_manager_subtask'
+        ordering = ['-created_at']
+        verbose_name = 'Subtask'
+
 
 
 class Category(models.Model):
